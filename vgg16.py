0ce01ecd2ef5422ef2ea65f6051321a08e5d2fae
EC2 Default User <ec2-user@ip-172-31-12-180.ap-southeast-2.compute.internal>
2017-09-04 05:44:18 +0000

    all

--- vgg16.py
+++ vgg16.py
@@ -16,9 +16,61 @@ from keras import backend as K
 K.set_image_dim_ordering('th')
 import sklearn.metrics as skm
 
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+def precision(y_true, y_pred):
+    """Precision metric.		
+
+    Only computes a batch-wise average of precision.		
+
+    Computes the precision, a metric for multi-label classification of		
+    how many selected items are relevant.		
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def recall(y_true, y_pred):
+    """Recall metric.		
+
+    Only computes a batch-wise average of recall.		
+
+    Computes the recall, a metric for multi-label classification of		
+    how many relevant items are selected.		
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def f1(y_true, y_pred):
+    def recall(y_true, y_pred):
+        """Recall metric.
+
+        Only computes a batch-wise average of recall.
+
+        Computes the recall, a metric for multi-label classification of
+        how many relevant items are selected.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+    def precision(y_true, y_pred):
+        """Precision metric.
+
+        Only computes a batch-wise average of precision.
+
+        Computes the precision, a metric for multi-label classification of
+        how many selected items are relevant.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall))
 
 def vgg16_model(img_rows, img_cols, channel=1, num_classes=None):
     """VGG 16 Model for Keras
@@ -80,11 +132,12 @@ def vgg16_model(img_rows, img_cols, channel=1, num_classes=None):
     model.add(Dense(1000, activation='softmax'))
 
     # Loads ImageNet pre-trained data
-    model.load_weights('imagenet_models/vgg16_weights_th_dim_ordering_th_kernels.h5')
+    model.load_weights('../models/vgg16_weights_th_dim_ordering_th_kernels.h5')
 
     # Truncate and replace softmax layer for transfer learning
     model.layers.pop()
-    for layer in model.layers: layer.trainable = False
+    # for layer in model.layers[:25]:
+    #     layer.trainable = False
     model.outputs = [model.layers[-1].output]
     model.layers[-1].outbound_nodes = []
     model.add(Dense(num_classes, activation='sigmoid'))
@@ -94,96 +147,16 @@ def vgg16_model(img_rows, img_cols, channel=1, num_classes=None):
     #    layer.trainable = False
 
     # Learning rate is changed to 0.001
-    sgd = SGD(lr=1e-5, decay=1e-6, momentum=0.9, nesterov=True)
+    sgd = SGD(lr=1e-4, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd,
                   loss='binary_crossentropy',
-                  metrics=['accuracy',precision, recall, f1])
+                  metrics=['accuracy', precision, recall, f1])
 
     return model
 
-class Metrics(keras.callbacks.Callback):
-    def __init__(self):
-        self.losses = []
-        self.precisions = []
-        self.recalls = []
-        self.count  = 0
-
-
-    def on_epoch_end(self, batch, logs={}):
-        self.losses.append(logs.get('loss'))
-        self.precisions.append(logs.get('precession'))
-        self.recalls.append(logs.get('recall'))
-
-        y_pred = self.model.predict(self.validation_data[0])
-        y_true = self.validation_data[1]
-
-        print(y_pred[0])
-        print(y_true[0])
-
-        precision, recall, threshold = skm.precision_recall_curve(y_true.flatten(), y_pred.flatten().round())
-
-        print(precision)
-        print(recall)
-        print(threshold)
-
-        self.precisions.append(precision)
-        self.recalls.append(recall)
-
-        #
-        # precision, recall, thresholds = skm.precision_recall_curve(y_true, y_pred)
-
-        plt.plot(precision, recall)
-        plt.title('Precision Recall epoch %d'%self.count)
-        plt.ylabel('loss')
-        plt.xlabel('recall')
-        plt.show()
-        plt.savefig('precision_recall %d .png'%self.count)
-
-        print(self.precisions)
-        print(self.recalls)
-
-        return
 
 
 
-def precision(y_true, y_pred):
-    """Precision metric.		
-
-    Only computes a batch-wise average of precision.		
-
-    Computes the precision, a metric for multi-label classification of		
-    how many selected items are relevant.		
-    """
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    return precision
-
-def recall(y_true, y_pred):
-    """Recall metric.		
-
-    Only computes a batch-wise average of recall.		
-
-    Computes the recall, a metric for multi-label classification of		
-    how many relevant items are selected.		
-    """
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    recall = true_positives / (possible_positives + K.epsilon())
-    return recall
-
-def f1(y_true, y_pred):
-    pre = precision(y_true, y_pred)
-    rec = recall(y_true, y_pred)
-    return 2*((pre*rec)/(pre+rec))
-
-def show_image(image_data, lables=""):
-    arr = np.ascontiguousarray(image_data.transpose(1, 2, 0))
-    img = Image.fromarray(arr, 'RGB')
-    # font = ImageFont.truetype("/Library/Fonts/Arial.ttf", 10)
-    # draw = ImageDraw.Draw(img)
-    # draw.text((0, 0), lables, (255, 255, 0), font=font)
-    img.show()
 
 
 if __name__ == '__main__':
@@ -194,41 +167,31 @@ if __name__ == '__main__':
     channel = 3
     num_classes = 20
     batch_size = 16 
-    nb_epoch = 10
+    nb_epoch = 60
 
     # Load Cifar10 data. Please implement your own load_data() module for your own dataset
     # X_train, Y_train, X_valid, Y_valid = load_cifar10_data(img_rows, img_cols)
     X_train, Y_train, X_valid, Y_valid = load_pascal_data("VOC2007")
 
-    i = 0
-    for img in X_train[:4]:
-        show_image(img, "".join(str(Y_train[i])))
-        i += 1
-
-    i = 2000
-    for img in X_valid[2000:2010]:
-        show_image(img, "".join(str(Y_valid[i])))
-        i += 1
 
     # Load our model
     model = vgg16_model(img_rows, img_cols, channel, num_classes)
 
-    metrics = Metrics()
-
     # Start Fine-tuning
     history = model.fit(X_train, Y_train,
               batch_size=batch_size,
-              nb_epoch=nb_epoch,
+              epochs=nb_epoch,
               shuffle=True,
               verbose=1,
               validation_data=(X_valid, Y_valid),
-              callbacks=[metrics],
               )
 
     model.save_weights('trained_model.h5')
 
     # Make predictions
     predictions_valid = model.predict(X_valid, batch_size=batch_size, verbose=1)
+    print(predictions_valid)
+
 
     # Cross-entropy loss score
     score = log_loss(Y_valid, predictions_valid)
@@ -240,7 +203,31 @@ if __name__ == '__main__':
     plt.title('model loss')
     plt.ylabel('loss')
     plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
+    plt.legend(['train_loss', 'val_loss'], loc='upper left')
     plt.show()
-    plt.savefig('loss epoch.png')
+    plt.savefig('losses.png')
+    plt.clf()
+
+    print(history.history)
+
+
+    plt.plot(history.history['precision'])
+    plt.plot(history.history['recall'])
+    plt.plot(history.history['f1'])
+    plt.title('train precision recall')
+    plt.ylabel('score')
+    plt.legend(['precision', 'recall', 'f1'], loc='upper left')
+    plt.show()
+    plt.savefig('train_precision_recall.png')
+    plt.clf()
+
+    plt.plot(history.history['val_precision'])
+    plt.plot(history.history['val_recall'])
+    plt.plot(history.history['val_f1'])
+    plt.title('val precision recall')
+    plt.ylabel('score')
+    plt.legend(['precision', 'recall', 'f1'], loc='upper left')
+    plt.show()
+    plt.savefig('val_precision_recall.png')
+    plt.clf()
 
