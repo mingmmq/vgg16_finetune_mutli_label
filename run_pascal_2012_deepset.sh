938e1818d1ea4474ce209b8181a97bc2582ad673
whoisever <mobileadelaide@gmail.com>
2017-09-26 17:11:33 +0930

    shell

--- run_pascal_2012_deepset.sh
+++ run_pascal_2012_deepset.sh
@@ -9,42 +9,63 @@ source ./save_images.sh
 
 
 
-fix="--gpu 1 --epochs 40 --data VOC2012 --grid 7"
+fix="--gpu 1 --epochs 20 --data VOC2012 --grid 7"
 
 #usage: vgg16.py
 # [-h] [--lr LR] [--grid GRID] [--epochs EPOCHS]
 # [--lw LW] [--rw RW] [--lf LF] [--af AF] [--data PV]
 
 
-#change to see the different learning rate
-text="--lr 1e-3 "
-python vgg16.py $text $fix | tee 2012deep.out
-para=$(echo $text $fix | tr -d ' ')
-save_images $para
+##change to see the different learning rate
+#text="--lr 1e-3 "
+#python vgg16.py $text $fix | tee 2012deep.out
+#para=$(echo $text $fix | tr -d ' ')
+#save_images $para
+#
+#text="--lr 1e-3 --lf yes"
+#python vgg16.py $text $fix | tee 2012deep.out
+#para=$(echo $text $fix | tr -d ' ')
+#save_images $para
 
-text="--lr 1e-3 --lf yes"
+text="--lr 1e-3 --lf yes --lw 10"
 python vgg16.py $text $fix | tee 2012deep.out
 para=$(echo $text $fix | tr -d ' ')
 save_images $para
+mv 2012deep.out $?
 
-text="--lr 1e-3 --lf yes --lw 300"
-python vgg16.py $text $fix | tee 2012deep.out
-para=$(echo $text $fix | tr -d ' ')
-save_images $para
 
-text="--lr 1e-3 --lf yes --rw 300"
+text="--lr 1e-3 --lf yes --lw 50"
 python vgg16.py $text $fix | tee 2012deep.out
 para=$(echo $text $fix | tr -d ' ')
 save_images $para
+mv 2012deep.out $?
 
-text="--lr 1e-3 --lf yes --lw 300 --rw 300"
+text="--lr 1e-3 --lf yes --lw 100"
 python vgg16.py $text $fix | tee 2012deep.out
 para=$(echo $text $fix | tr -d ' ')
 save_images $para
-text="--lr 1e-4 "
+mv 2012deep.out $?
+
+text="--lr 1e-3 --lf yes --lw 200"
 python vgg16.py $text $fix | tee 2012deep.out
 para=$(echo $text $fix | tr -d ' ')
 save_images $para
+mv 2012deep.out $?
+
+#
+#text="--lr 1e-3 --lf yes --rw 300"
+#python vgg16.py $text $fix | tee 2012deep.out
+#para=$(echo $text $fix | tr -d ' ')
+#save_images $para
+#
+#text="--lr 1e-3 --lf yes --lw 300 --rw 300"
+#python vgg16.py $text $fix | tee 2012deep.out
+#para=$(echo $text $fix | tr -d ' ')
+#save_images $para
+#text="--lr 1e-4 "
+#python vgg16.py $text $fix | tee 2012deep.out
+#para=$(echo $text $fix | tr -d ' ')
+#save_images $para
 
 #custom loss function with default weights
 text="--lr 1e-4 --lf yes"
