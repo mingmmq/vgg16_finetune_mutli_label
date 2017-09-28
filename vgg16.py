c021d7620f22ea49eef651ae269852c3a1fcb385
whoisever <mobileadelaide@gmail.com>
2017-09-28 16:57:21 +0930

    update sample number

--- vgg16.py
+++ vgg16.py
@@ -193,11 +193,11 @@ def _loss_tensor_bak(y_true, y_pred):
 def _loss_tensor(y_true, y_pred):
     y_pred = K.clip(y_pred, K.epsilon(), 1.0-K.epsilon())
 
-    if pa.random_sample:
+    if pa.random_sample != None:
         #find the 1 labels sum in each row
         sum_of_each = K.round(K.sum(y_true, axis=1))
         #get 3 times of the to keep the data
-        keep_of_each = sum_of_each * pa.random_sample
+        keep_of_each = sum_of_each * 10
         #get the max of these number
         max = K.max(keep_of_each)
         #get the shape of y_true
